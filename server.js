96cf258da23217f2741b1d0791a3b4f9404c853f
Jason McCoy <jwmccoy29@gmail.com>
2022-05-17 11:01:21 -0500

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-const potteryController = require('./controller/PotteryController')
+const {
+  getPotteryDetails,
+  createPotteryDetails,
+  getTypeDetails,
+  getPotteryByTypes,
+  updatePottery,
+  deletePottery
+} = require('./controller/PotteryController')
 const db = require('./db')
 const logger = require('morgan')
 
@@ -8,23 +15,19 @@ const app = express()
 
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(`${__dirname}/client/build`))
 app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: false }))
 
 //routes
-app.post('/create', potteryController.createPotteryDetails)
-app.get('/types/:types', potteryController.getPotteryByTypes)
-app.put('/update/:id', potteryController.updatePottery)
-app.delete('/delete/:id', potteryController.deletePottery)
-app.get('/types', potteryController.getTypeDetails)
-app.get('/', potteryController.getPotteryDetails)
+app.post('/create', createPotteryDetails)
+app.get('/types/:types', getPotteryByTypes)
+app.put('/update/:id', updatePottery)
+app.delete('/delete/:id', deletePottery)
+app.get('/types', getTypeDetails)
+app.get('/', getPotteryDetails)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/client/build/index.html`)
-})
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
